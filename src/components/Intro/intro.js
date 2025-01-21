d812caf1c72ce34a9f9e57d9dbc0f5011f922098
Varad Jagdale <vivjaggs.39@gmail.com>
2025-01-21 11:50:21 +0530

    changes

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -8,7 +8,7 @@ const Intro = () => {
       <div className="introContent">
         <span className="hello">Hello,</span>
         <span className="introText">I'm <span className="introName">Varad</span> <br />Full Stack Developer</span>
-        <p className="introPara">"Driven by a passion for technology and problem-solving, I specialize in creating modern, scalable web applications using Python, Java, and the MERN stack. With expertise in API integration, database optimization, and responsive design, I strive to deliver impactful solutions through teamwork and innovation."</p>
+        <p className="introPara">Recent graduate with a passion for technology and problem-solving. Skilled in full-stack development and eager to join to gain real-world IT experience, refine my technical skills, and contribute effectively to innovative projects.</p>
         <Link to="contactPage" smooth={true} offset={-50} duration={500} ><button className="btn"><img src="hiremelogo.png" alt="Hire Me" className="btnImg" />Hire Me</button></Link>
         {/* <img src="employee.png" alt="Profile" className="bg"/> */}
       </div>
